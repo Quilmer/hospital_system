b10ee380a83ec0416a6fdb8c3e0ad39d25c86aa4
unknown <luedihg@gmail.com>
2026-04-17 01:46:22 +0100

    Modificação na comunicação

--- database.py
+++ database.py
@@ -1,22 +1,22 @@
 import mysql.connector
 
 def get_connection():
-    return mysql.connector.connect(
+    connection = mysql.connector.connect(
         host="127.0.0.1",
         user="root",
         password="",
         database="hospital_db",
         autocommit=True     # ⚠️ faz o autoteste para evitar esquecer de dar commit()
     )
-    return conn
+    return connection
 
 # print("Ficheiro executado!")  
 
-# if __name__ == "__main__":
-#    print("Entrou no teste!")  # linha de teste
-#    try:
-#        conn = get_connection()
-#        print("Ligação à base de dados OK!")
-#        conn.close()
-#    except Exception as e:
-#        print("Erro:", e)
+if __name__ == "__main__":
+    print("Entrou no teste!")  # linha de teste
+    try:
+        conn = get_connection()
+        print("Ligação à base de dados OK!")
+        conn.close()
+    except Exception as e:
+        print("Erro:", e)
